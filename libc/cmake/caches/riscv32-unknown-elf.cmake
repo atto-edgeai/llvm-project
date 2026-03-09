cd639c1a9a41bfa735926939e18887cb6b9f9d1a
Djordje Todorovic <djolertrk@gmail.com>
2026-03-09 07:52:22 +0100

    Fix libc/cmake/caches/riscv32-unknown-elf.cmake

--- libc/cmake/caches/riscv32-unknown-elf.cmake
+++ libc/cmake/caches/riscv32-unknown-elf.cmake
@@ -1,4 +1,8 @@
 set(CMAKE_SYSTEM_PROCESSOR RISCV CACHE STRING "")
 set(RUNTIMES_TARGET_TRIPLE "riscv32-unknown-elf" CACHE STRING "")
 
+foreach(lang C;CXX;ASM)
+    set(CMAKE_${lang}_FLAGS "-Wno-atomic-alignment" CACHE STRING "")
+endforeach()
+
 include(${CMAKE_CURRENT_LIST_DIR}/baremetal_common.cmake)
